7823305fbc68b40e98cbaf5882e6b5f6b9d9988e
Chris Burr <christopher.burr@cern.ch>
2020-06-27 14:29:11 +0200

    Update to 5.39 and merge with libmagic

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,13 @@
-./configure --prefix=$PREFIX
-make -j $CPU_COUNT
+#!/usr/bin/env bash
+
+./configure \
+    --prefix="${PREFIX}" \
+    --datadir="${PREFIX}/share" \
+    --disable-silent-rules \
+    --disable-dependency-tracking
+
+make "-j${CPU_COUNT}"
+
 make check
+
 make install
